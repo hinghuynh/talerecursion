574c4515bbe7ad6b7546826f1e054a37678e5442
hinghuynh <hinghuynh@gmail.com>
2014-07-28 15:10:29 -0500

    story loads

--- FinishedTableViewController.m
+++ FinishedTableViewController.m
@@ -123,9 +123,11 @@
         
         [segue.destinationViewController setHidesBottomBarWhenPushed:YES];
         StoryViewController *storyViewController = (StoryViewController *)segue.destinationViewController;
-
+        double delayInSeconds = 0.2;
+        dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW, (int64_t)(delayInSeconds * NSEC_PER_SEC));
+        dispatch_after(popTime, dispatch_get_main_queue(), ^(void){
         storyViewController.message = self.joinedString;
-        
+         });
     }
 }
 
--- StoryViewController.m
+++ StoryViewController.m
@@ -17,10 +17,18 @@
 
 
 - (void)viewDidLoad
+
 {
     [super viewDidLoad];
+    double delayInSeconds = 0.5;
+    dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW, (int64_t)(delayInSeconds * NSEC_PER_SEC));
+    dispatch_after(popTime, dispatch_get_main_queue(), ^(void){
+ 
+
+  
     NSLog(@"PLEASE SHOW %@", self.message);
     self.storyLabel.text = self.message;
+});
 }
 
 
