2354d071d94291b4bf0bff835ec8d652d63ae6f7
absarora <absingharora@gmail.com>
2014-07-30 09:00:43 -0500

    added missing files

---
+++ TaleRecursion/InboxViewController.h
@@ -0,0 +1,19 @@
+//
+//  InboxViewController.h
+//  TaleRecursion
+//
+//  Created by Hing Huynh on 7/27/14.
+//  Copyright (c) 2014 Appcoders. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <Parse/Parse.h>
+
+@interface InboxViewController : UITableViewController
+
+@property (nonatomic, strong) NSArray *messages;
+@property (nonatomic, strong) PFObject *selectedMessage;
+
+- (IBAction)logout:(id)sender;
+
+@end
---
+++ TaleRecursion/LoginViewController.h
@@ -0,0 +1,18 @@
+//
+//  LoginViewController.h
+//  TaleRecursion
+//
+//  Created by Hing Huynh on 7/27/14.
+//  Copyright (c) 2014 Appcoders. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController : UIViewController
+
+@property (strong, nonatomic) IBOutlet UITextField *usernameField;
+@property (strong, nonatomic) IBOutlet UITextField *passwordField;
+
+- (IBAction)login:(id)sender;
+
+@end
